6b340eea14afec781e87ef91ee70d830d8cacbb7
Karan Desai <karan.desai@promactinfo.com>
2016-07-02 13:54:04 +0530

    Linking demo app files

---
+++ MVCFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic; //define generic collections
+using System.Linq; //language integrated query
+using System.Web;  //to read http values
+using System.Web.Mvc; //supports mvc framework
+
+namespace MVCFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Mycontroller()
+        {
+            return "Hi, I am a controller";
+        }
+        public ActionResult MyView()
+        {
+            return View();
+        }
+       
+    }
+}
+//controllername/actionname/idparameter
